ae038de754a18a7721498c1c2c9f7e5e0c23ac2e
favilo <kevin.oberlies@elastic.co>
2024-03-08 16:46:44 -0800

    Handle Init separately

--- src/bin/unique-ids.rs
+++ src/bin/unique-ids.rs
@@ -11,8 +11,12 @@ pub enum Payload {
     Init(vorticity::Init),
     InitOk,
 
-    Echo { echo: String },
-    EchoOk { echo: String },
+    Generate,
+
+    GenerateOk {
+        #[serde(rename = "id")]
+        guid: String,
+    },
 }
 
 impl vorticity::Payload for Payload {
@@ -28,45 +32,50 @@ impl vorticity::Payload for Payload {
     }
 }
 
-pub struct EchoNode {
-    pub id: usize,
+pub struct UniqueNode {
+    pub msg_id: usize,
+    pub node: String,
 }
 
-impl Node<(), Payload> for EchoNode {
+impl Node<(), Payload> for UniqueNode {
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
-            Payload::Echo { echo } => {
+            Payload::Generate => {
+                let guid = format!("{}-{}", self.node, self.msg_id);
                 let reply = Message {
                     src: input.dst,
                     dst: input.src,
                     body: Body {
-                        id: Some(self.id),
+                        id: Some(self.msg_id),
                         in_reply_to: input.body.id,
-                        payload: Payload::EchoOk { echo },
+                        payload: Payload::GenerateOk { guid },
                     },
                 };
 
                 serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to echo")?;
+                    .context("serialize response to generate")?;
                 output.write_all(b"\n").context("write newline to output")?;
             }
-            Payload::EchoOk { .. } => {}
             Payload::Init { .. } => bail!("init should already be handled"),
             Payload::InitOk => bail!("Unexpected InitOk message"),
+            Payload::GenerateOk { .. } => bail!("Unexpected GenerateOk message"),
         }
-        self.id += 1;
+        self.msg_id += 1;
 
         Ok(())
     }
 
-    fn from_init(_state: (), _init: vorticity::Init) -> anyhow::Result<Self>
+    fn from_init(_state: (), init: vorticity::Init) -> anyhow::Result<Self>
     where
         Self: Sized,
     {
-        Ok(Self { id: 0 })
+        Ok(Self {
+            msg_id: 1,
+            node: init.node_id,
+        })
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop::<_, EchoNode, Payload>(())
+    main_loop::<(), UniqueNode, Payload>(())
 }
