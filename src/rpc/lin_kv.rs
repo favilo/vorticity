3d2bbe79454e7dcb895bdef25e9a72c81da2f5ac
favilo <kevin.oberlies@elastic.co>
2024-04-13 16:29:23 -0700

    Clean up functions with fewer arguments

--- src/rpc/lin_kv.rs
+++ src/rpc/lin_kv.rs
@@ -3,7 +3,7 @@ use std::{any::Any, cell::RefCell};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-use super::{CallbackInfo, CallbackStatus};
+use super::{CallbackInfo, CallbackStatus, RpcCallback};
 use crate::{
     error::{Error, Result},
     message::{MessageSet, ToEvent},
@@ -18,27 +18,40 @@ pub struct LinKv {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
-pub enum LinKvPayload {
-    Read {
-        key: String,
-    },
-    ReadOk {
-        value: Value,
-    },
+pub struct Read {
+    pub key: String,
+}
 
-    Write {
-        key: String,
-        value: Value,
-    },
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub struct Write {
+    pub key: String,
+    pub value: Value,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub struct Cas {
+    pub key: String,
+    pub from: Value,
+    pub to: Value,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub create_if_not_exists: Option<bool>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum LinKvPayload {
+    Read(Read),
+    ReadOk { value: Value },
+
+    Write(Write),
     WriteOk,
 
-    Cas {
-        key: String,
-        from: Value,
-        to: Value,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        create_if_not_exists: Option<bool>,
-    },
+    Cas(Cas),
     CasOk,
 }
 
@@ -60,16 +73,15 @@ impl Handler for LinKv {
             return Err(Error::WrongEvent(json));
         };
 
-        // handle responses here. We are going to actually be sending in methods
-        // TODO: figure out errors
+        // TODO: figure out errors from lin-kv cas, when there is contention
         match &event.body().payload {
-            LinKvPayload::ReadOk { .. } => {
-                self.handle_read_ok(&event, ctx)?;
+            LinKvPayload::ReadOk { .. } | LinKvPayload::WriteOk | LinKvPayload::CasOk => {
+                self.handle_reply(&event, ctx)?
             }
-            LinKvPayload::WriteOk => todo!(),
-            LinKvPayload::CasOk => todo!(),
 
-            LinKvPayload::Write { .. } | LinKvPayload::Cas { .. } | LinKvPayload::Read { .. } => {}
+            LinKvPayload::Write(Write { .. })
+            | LinKvPayload::Cas(Cas { .. })
+            | LinKvPayload::Read(Read { .. }) => {}
         };
 
         Ok(())
@@ -77,9 +89,11 @@ impl Handler for LinKv {
 }
 
 pub type ReadCallback<Payload> = dyn Fn(Message<Payload>, &mut dyn Any, Value, Context);
+pub type WriteCallback<Payload> = dyn Fn(Message<Payload>, &mut dyn Any, Context);
+pub type CasCallback<Payload> = dyn Fn(Message<Payload>, &mut dyn Any, Context);
 
 impl LinKv {
-    fn handle_read_ok(&self, event: &Message<LinKvPayload>, ctx: Context) -> Result<()> {
+    fn handle_reply(&self, event: &Message<LinKvPayload>, ctx: Context) -> Result<()> {
         let mut borrow = self.callbacks.borrow_mut();
         let (idx, callback_info) = borrow
             .iter_mut()
@@ -99,7 +113,7 @@ impl LinKv {
     // TODO: Handle timeouts
     pub fn read<Payload>(
         &self,
-        key: &str,
+        read: Read,
         orig_msg: &Message<Payload>,
         state: Box<dyn Any>,
         callback: Box<ReadCallback<Payload>>,
@@ -121,19 +135,107 @@ impl LinKv {
             Ok(CallbackStatus::Finished)
         };
 
+        self.persist_callback(
+            ctx,
+            LinKvPayload::Read(read),
+            orig_msg,
+            state,
+            Box::new(process_callback),
+        )?;
+
+        Ok(())
+    }
+
+    pub fn write<Payload>(
+        &self,
+        write: Write,
+        orig_msg: &Message<Payload>,
+        state: Box<dyn Any>,
+        callback: Box<WriteCallback<Payload>>,
+        ctx: Context,
+    ) -> Result<()>
+    where
+        Payload: Clone + Serialize + for<'de> Deserialize<'de> + 'static,
+    {
+        let process_callback = move |orig_msg: &Message<Value>,
+                                     state: &mut dyn Any,
+                                     _set: &mut MessageSet<LinKvPayload>,
+                                     msg: &Message<LinKvPayload>,
+                                     ctx: Context|
+              -> Result<CallbackStatus> {
+            let LinKvPayload::WriteOk = msg.body().payload.clone() else {
+                return Err(Error::WrongEvent(ToEvent::Message(msg.to_value())));
+            };
+            callback(Message::to_payload(orig_msg)?, state, ctx);
+            Ok(CallbackStatus::Finished)
+        };
+
+        self.persist_callback(
+            ctx,
+            LinKvPayload::Write(write),
+            orig_msg,
+            state,
+            Box::new(process_callback),
+        )?;
+
+        Ok(())
+    }
+
+    pub fn cas<Payload>(
+        &self,
+        cas: Cas,
+        orig_msg: &Message<Payload>,
+        state: Box<dyn Any>,
+        callback: Box<CasCallback<Payload>>,
+        ctx: Context,
+    ) -> Result<()>
+    where
+        Payload: Clone + Serialize + for<'de> Deserialize<'de> + 'static,
+    {
+        let process_callback = move |orig_msg: &Message<Value>,
+                                     state: &mut dyn Any,
+                                     _set: &mut MessageSet<LinKvPayload>,
+                                     msg: &Message<LinKvPayload>,
+                                     ctx: Context|
+              -> Result<CallbackStatus> {
+            let LinKvPayload::CasOk = msg.body().payload.clone() else {
+                return Err(Error::WrongEvent(ToEvent::Message(msg.to_value())));
+            };
+            callback(Message::to_payload(orig_msg)?, state, ctx);
+            Ok(CallbackStatus::Finished)
+        };
+
+        self.persist_callback(
+            ctx,
+            LinKvPayload::Cas(cas),
+            orig_msg,
+            state,
+            Box::new(process_callback),
+        )?;
+
+        Ok(())
+    }
+
+    fn persist_callback<Payload>(
+        &self,
+        ctx: Context,
+        payload: LinKvPayload,
+        orig_msg: &Message<Payload>,
+        state: Box<dyn Any>,
+        process_callback: Box<RpcCallback<LinKvPayload>>,
+    ) -> Result<()>
+    where
+        Payload: Clone + Serialize + for<'de> Deserialize<'de> + 'static,
+    {
         let msg_set = MessageSet::new(&[Message::builder(ctx.clone())
             .dst("lin-kv")
             .id(ctx.clone())
-            .payload(LinKvPayload::Read {
-                key: key.to_owned(),
-            })
+            .payload(payload)
             .build()?]);
         ctx.send_set(&msg_set)?;
         let callback_info =
             CallbackInfo::new(orig_msg.clone(), state, msg_set, Box::new(process_callback));
-
         self.callbacks.borrow_mut().push(callback_info);
-
         Ok(())
     }
 }
